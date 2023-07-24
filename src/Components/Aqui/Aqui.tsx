10170fdc78e170b05c72fcef746f7f9726a0912c
Dominique <carinefrvga@gmail.com>
2023-07-24 17:01:22 -0300

    aqui voce navega section

---
+++ src/Components/Aqui/Aqui.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import image from "../../Assets/man-phone.png"
+
+import "./aqui.css"
+
+function Aqui() {
+  return (
+    
+<section className='aqui'>
+  <div className='aqui-texts'>
+
+    <h1>Atendemos a sua necessidade, navegue conosco!</h1>
+    <h1>GoodNet, aqui você navega!</h1>
+  </div>
+
+  <img src={image} alt="" />
+
+  </section>
+  )
+}
+
+export default Aqui
