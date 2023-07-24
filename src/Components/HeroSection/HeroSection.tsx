ec805f4cee2898669c61c60b1cbdd72475b5faa4
Dominique <carinefrvga@gmail.com>
2023-07-24 17:29:18 -0300

    edit: hero section

--- src/Components/HeroSection/HeroSection.tsx
+++ src/Components/HeroSection/HeroSection.tsx
@@ -1,57 +1,76 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './heroSection.css'
 import heroimg from "./../../Assets/image.png"
 import background from "./../../Assets/background.png";
 
+
 function HeroSection() {
+
+  const [height, setHeight] = useState(0)
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if(ref.current){
+      setHeight(ref.current.clientHeight)
+    }
+  }, [])
+
   return (
     <div style={{ 
-      backgroundImage: `url(${background})`,backgroundSize: 'cover',
+      position: 'relative',
+      backgroundImage: `url(${background})`,
+      backgroundSize: 'cover',
       backgroundRepeat: 'no-repeat'
     }}>
 
-    <section  className='home container flex center-vertical'>
+      <section  className='home container flex center-vertical'>
 
-      <div className='column left
-      '>
+        <div className='column left'>
           <h2 className='s'>Internet 100% fibra óptica</h2>
-          <h1 className='main-text'>Procurando Internet de qualidade para sua casa?
-            </h1>
+          <h1 className='main-text'>Procurando Internet de qualidade para sua casa?</h1>
             
-          {/* <h1 className='main-text'>Nós queremos revolucionar a qualidade da sua Internet</h1> */}
-          {/* color:'#ed6709' */}
-          {/* <h2 style={{marginTop: '15px', fontSize:'25px'}} className='s'>Ultra</h2> */}
-          <h1 style={{fontSize: '95px'}}>200 <span style={{fontSize: '35px'}}>Mb</span></h1>
-          {/* <h1 style={{fontSize: '35px', marginTop:'0px'}}>Mb</h1> */}
-          {/* <p className='secondary-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, repellendus perspiciatis</p> */}
 
-          <div className='hero-preco'>
-            <p className='m1'>R$</p>
-            <p className='m'>99</p>
-            <p className='m'>,90</p>
-            <p className='m2'>/mês</p>
+          <div className='a'>
+            <h1>200</h1>
+
+            <div className='b'>   
+              <span style={{fontSize: '45px'}}>MEGA</span>
+              <span style={{fontSize: '30px', fontWeight: '300'}}>Download</span>
+            </div>
+
           </div>
 
+          <div className="n">
 
-            <a className='btn' href="#">
-              Contratar agora
-            </a>
-
-      </div>
-
-      <div className='column right'>
-          
-          <img className='hero-img' src={heroimg} alt="" />
-
+            <div className='hero-preco'>
+              <p className='m1'>R$</p>
+              <p className='m'>99,90</p>
+              <p className='m2'>/mês</p>
+            </div>
         
+            <div className="hero-btn-container">
+
+              <a className='hero-btn' href="#">
+                Contratar agora
+              </a>
+            </div>
+
+          </div>
+
+        </div>
+
+        <div className='column right'>
+            <img className='hero-img' src={heroimg} alt="" />
+        </div>
+
+      </section>
+      
+      <div ref={ref} style={{bottom: -height/2}} className='banner' >
+        <h1 style={{fontWeight:'300'}}>Não fique desconectado, garanta já sua Internet!</h1>
       </div>
-
-        
-
-    </section>
     </div>
 
   )
 }
-
+// 
 export default HeroSection
