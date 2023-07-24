b4aa96941a236ab69fa81555ed7695299f43e2b7
Dominique <carinefrvga@gmail.com>
2023-07-24 17:01:59 -0300

    add footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./footer.css"
+import {FaFacebook, FaYoutube, FaInstagram, FaLink} from "react-icons/fa"
 
 function Footer() {
   return (
-    <footer className='footer'>Footer</footer>
+    <footer className='footer'>
+      <ul className='footer-social-medias'>
+        <li className='footer-list-item'><a href="#"><FaFacebook aria-label='Facebook'/></a></li>
+        <li className='footer-list-item'><a href="#"><FaYoutube aria-label='Youtube'/></a></li>
+        <li className='footer-list-item'><a href="#"><FaInstagram aria-label='Instagram'/></a></li>
+        <li className='footer-list-item'><a href="https://perficio.com.br/goodnet/central"><FaLink aria-label='Perficio/GoodNet'/></a></li>
+      </ul>
+      <h3>Travessa Lauro de Freitas, 45 - 1º Andar/Sala 03, Centro, Juazeiro - BA, 48903-465</h3>
+      <p>&copy;2020 - <a href="#">GoodNet</a> - Todos os direitos reservados.</p>
+
+    </footer>
   )
 }
 
